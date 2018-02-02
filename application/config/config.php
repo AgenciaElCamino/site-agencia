70c7007f6b4fa66406d63fe47a1326026623ee5d
Fernando Soares <fernandosoares.dev@gmail.com>
2018-02-02 11:56:30 -0200

    Codeigniter encription key

--- application/config/config.php
+++ application/config/config.php
@@ -324,7 +324,7 @@ $config['cache_query_string'] = FALSE;
 | https://codeigniter.com/user_guide/libraries/encryption.html
 |
 */
-$config['encryption_key'] = '';
+$config['encryption_key'] = 'VcKqL22D6fm6pwT0S4vbxuNGvN9dUcwm';
 
 /*
 |--------------------------------------------------------------------------
